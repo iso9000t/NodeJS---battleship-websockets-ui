92b0b40a66ab4894f2d53ad22db29224823b643d
iso9000t <sasha.gr81@gmail.com>
2024-02-24 23:07:08 -0300

    feat: add password validation

--- src/handlers/registrationHandler.ts
+++ src/handlers/registrationHandler.ts
@@ -2,32 +2,55 @@ import { WebSocket } from 'ws';
 import { Command, WebSocketClient } from '../models/models';
 import { database } from '../database/database';
 // Assuming updateWinners is exported from where it's defined
-import { updateWinners } from './updatewinners';
+import { updateWinners } from './updateWinners';
 import { updateRoom } from './updateRoomHandler';
+
 export function handleRegistration(ws: WebSocket, command: Command, wss) {
-  // If wss is passed as a parameter
   console.log('Received registration command:', command);
 
   const wsClient = ws as unknown as WebSocketClient;
-
   const requestData = JSON.parse(command.data);
   console.log('Registration requestData:', requestData);
 
   const existingPlayer = database.findPlayerByName(requestData.name);
+
   if (existingPlayer) {
-    wsClient.send(
-      JSON.stringify({
-        type: 'reg',
-        data: JSON.stringify({
-          error: true,
-          errorText: 'Player already exists.',
-        }),
-        id: command.id,
-      })
-    );
-    return;
+    // Player exists, check password
+    if (existingPlayer.password === requestData.password) {
+      // Password matches, player is logging back in
+      wsClient.index = existingPlayer.index;
+      wsClient.name = existingPlayer.name;
+      database.linkConnectionToPlayer(wsClient, existingPlayer.index);
+
+      wsClient.send(
+        JSON.stringify({
+          type: 'reg',
+          data: JSON.stringify({
+            error: false,
+            message: 'Welcome back!',
+            name: existingPlayer.name,
+            index: existingPlayer.index,
+          }),
+          id: command.id,
+        })
+      );
+    } else {
+      // Password does not match
+      wsClient.send(
+        JSON.stringify({
+          type: 'reg',
+          data: JSON.stringify({
+            error: true,
+            errorText: 'Incorrect password.',
+          }),
+          id: command.id,
+        })
+      );
+    }
+    return; // Stop further processing
   }
 
+  // If the player doesn't exist, add them as new
   const newPlayer = database.addPlayer(requestData.name, requestData.password);
   wsClient.index = newPlayer.index;
   wsClient.name = newPlayer.name;
